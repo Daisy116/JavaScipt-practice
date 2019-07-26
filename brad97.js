215a88f8bfa91fefc338c24ab1c69349ddbad9f0
Daisy116 <52897966+Daisy116@users.noreply.github.com>
2019-07-26 16:13:07 +0800

    Add files via upload
    2019_07_26 Note

--- brad97.js
+++ brad97.js
@@ -1,8 +1,13 @@
 //產生三位數，可以零開頭，三個數字都不同
 let logtext = '';
 let times = 0;     //設為全域變數
+let answer;
+let guslength = 3;
 
-function createAnswer(n = 3){    //預設猜三位數
+answer = createAnswer(guslength);
+console.log(answer);
+
+function createAnswer(guslength = 3){    //預設猜三位數
     let poker = [];
     for(let i=0; i<10; i++) poker[i] = i;
     for(let i = poker.length-1 ; i>0; i--){
@@ -11,7 +16,7 @@ function createAnswer(n = 3){    //預設猜三位數
     }
 
     let ret = '';   //ret是字串，所以ret +=  才能執行!!
-    for(let i = 0;i<n;i++){
+    for(let i = 0; i<guslength; i++){
         ret += poker[i];
     }
     return ret;
@@ -41,7 +46,7 @@ function doGuess(){
     document.getElementById('log').innerHTML = logtext;
     gusnum = " ";    
 
-    if(result == `${d}A0B`){
+    if(result == `${guslength}A0B`){
         alert(`YOU WIN`);
     }else if(times >= 10){
         alert(`Loser: answer = ${answer}`);
@@ -50,10 +55,24 @@ function doGuess(){
 }
 
 function replay(){    //為何不會重新產生答案??
-    answer = createAnswer();
+    answer = createAnswer(changeLength);
     logtext = "";
     times = 0;
     document.getElementById('log').innerHTML = logtext;
     document.getElementById('input').value = logtext;
     document.getElementById('input').focus();
+}
+
+function changeLength(){
+    let op = document.getElementById("guslength");
+    if(op.value == "3"){
+        guslength = 3;
+    }else if(op.value == "4"){
+        guslength = 4;
+    }else if(op.value == "5"){
+        guslength = 5;     
+    }
+    console.log(guslength);
+    answer = createAnswer(guslength);
+    console.log(answer);
 }
